adec3278f93620308439af189405547c72bd1bfa
Jaime Muñoz <jaime.munoz200@gmail.com>
2022-08-24 10:15:02 -0600

    COnfiguracion de .env yarn add @nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,18 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ServeStaticModule.forRoot({
     rootPath: join(__dirname,'..','public'),
     }),
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot(process.env.MONGODB),
     PokemonModule,
     CommonModule,
     SeedModule,
@@ -19,4 +21,8 @@ import { SeedModule } from './seed/seed.module';
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(){
+    
+  }
+}
